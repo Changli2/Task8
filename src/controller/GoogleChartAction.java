04a2af445d5dbe7bc8c23a5ad659eaed7a8a311e
aayushc <aayush250@yahoo.co.in>
2015-02-17 23:46:51 -0500

    request for visit

--- src/controller/GoogleChartAction.java
+++ src/controller/GoogleChartAction.java
@@ -4,14 +4,16 @@ import javax.servlet.http.HttpServletRequest;
 
 import model.LocationDAO;
 import model.Model;
+import model.VisitDAO;
 
 import org.genericdao.RollbackException;
 
 import databean.Location;
+import databean.VisitBean;
 
 public class GoogleChartAction extends Action{
 	private LocationDAO locationDAO;
-		
+	private VisitDAO visitDAO;	
 		public String getName() {
 			// TODO Auto-generated method stub
 			return "googleChart.do";
@@ -20,15 +22,20 @@ public class GoogleChartAction extends Action{
 		public GoogleChartAction(Model model) {
 			// TODO Auto-generated constructor stub
 			locationDAO= model.getLocationDAO();
+			visitDAO= model.getVisitDAO();
 		}
 
 		@Override
 		public String perform(HttpServletRequest request) {
 			
 			Location[] locs;
+			VisitBean[] visits;
 			try {
 				locs = locationDAO.getTopTen();
 				request.setAttribute("topten", locs);
+				
+				visits=visitDAO.getRecord();
+				request.setAttribute("visits", visits);
 			} catch (RollbackException e) {
 				e.printStackTrace();
 			}
